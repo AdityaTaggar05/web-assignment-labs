9c9453f53b143bbde1f8b3af878278943b072720
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-03-21 15:24:04 +0530

    [fix] update logic for sidebar popup

--- components/toolbar/toolbar.js
+++ components/toolbar/toolbar.js
@@ -1,10 +1,10 @@
 const tools = document.querySelectorAll(".tool");
 tools.forEach((tool) => {
   tool.addEventListener("click", () => {
-    if (tool.classList.contains("active")) return;
-
-    tools.forEach((t) => t.classList.remove("active"));
-    tool.classList.add("active");
+    if (!tool.classList.contains("active")) {
+      tools.forEach((t) => t.classList.remove("active"));
+      tool.classList.add("active");
+    }
 
     document.querySelector(".sidebar").classList.add("active");
   });
