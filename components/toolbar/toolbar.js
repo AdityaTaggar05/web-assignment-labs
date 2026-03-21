87050af9efcf9aa4be8eed3073fd92a62fce8015
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-03-21 14:44:22 +0530

    restructure html/css to incorporate sidebar

--- components/toolbar/toolbar.js
+++ components/toolbar/toolbar.js
@@ -3,11 +3,9 @@ tools.forEach((tool) => {
   tool.addEventListener("click", () => {
     if (tool.classList.contains("active")) return;
 
-    if (tool.getAttribute("data-action") == "collapse") {
-      document.querySelector(".toolbar").classList.remove("active");
-    } else {
-      tools.forEach((t) => t.classList.remove("active"));
-      tool.classList.add("active");
-    }
+    tools.forEach((t) => t.classList.remove("active"));
+    tool.classList.add("active");
+
+    document.querySelector(".sidebar").classList.add("active");
   });
 });
