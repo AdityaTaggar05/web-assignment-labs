04041f663c662b61544afd1ed68b7634aaba953c
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-03-24 12:48:01 +0530

    [ui] add ui for theme toggle bar

--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -7,3 +7,17 @@ document.querySelector(".sidebar #close").addEventListener("click", () => {
     sidebar.classList.add("active");
   }
 });
+
+document
+  .querySelector(".sidebar .theme-toggle")
+  .addEventListener("click", () => {
+    const items = document.getElementsByClassName("theme-mode");
+
+    if (items[0].classList.contains("active")) {
+      items[0].classList.remove("active");
+      items[1].classList.add("active");
+    } else {
+      items[1].classList.remove("active");
+      items[0].classList.add("active");
+    }
+  });
