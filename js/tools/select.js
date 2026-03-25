7fa2b324ef3ce256ded5b5d58b88a6558cc57c88
AdityaTaggar05 <aditya05.mgg@gmail.com>
2026-03-25 11:39:35 +0530

    [select] fix element translation only on mouse drag and not on move

--- js/tools/select.js
+++ js/tools/select.js
@@ -1,4 +1,11 @@
 import { Tool } from "./tool.js";
+import { PencilTool } from "./pencil.js";
+import { RectangleTool } from "./rectangle.js";
+import { CircleTool } from "./circle.js";
+import { EllipseTool } from "./ellipse.js";
+import { TriangleTool } from "./triangle.js";
+import { TextTool } from "./text.js";
+import { renderSidebar } from "../sidebar.js";
 
 export class SelectTool extends Tool {
   constructor(stateManager) {
@@ -18,14 +25,19 @@ export class SelectTool extends Tool {
   }
 
   onMouseDown(e, ctx) {
-    if (this.selectedElement) {
+    if (
+      this.selectedElement &&
+      !this.selectedElement.isTargetted(e.offsetX, e.offsetY)
+    ) {
       this.selectedElement.isSelected = false;
       this.selectedElement = null;
       this.stateManager.render();
+      renderSidebar(this);
       this.stateManager.storeElements();
       return;
     }
 
+    this.mouseDown = true;
     for (const elem of this.stateManager.elements.toReversed()) {
       if (elem.isTargetted(e.offsetX, e.offsetY, ctx)) {
         this.selectedElement = elem;
@@ -33,11 +45,37 @@ export class SelectTool extends Tool {
       }
     }
 
-    this.stateManager.render();
+    if (this.selectedElement) {
+      this.stateManager.render();
+
+      let tool = null;
+      switch (this.selectedElement.type) {
+        case "pencil":
+          tool = new PencilTool(this.selectedElement.properties);
+          break;
+        case "rectangle":
+          tool = new RectangleTool(this.selectedElement.properties);
+          break;
+        case "circle":
+          tool = new CircleTool(this.selectedElement.properties);
+          break;
+        case "ellipse":
+          tool = new EllipseTool(this.selectedElement.properties);
+          break;
+        case "triangle":
+          tool = new TriangleTool(this.selectedElement.properties);
+          break;
+        case "text":
+          tool = new TextTool(this.selectedElement.properties);
+          break;
+      }
+
+      if (tool) renderSidebar(tool);
+    }
   }
 
   onMouseMove(e, ctx) {
-    if (this.selectedElement) {
+    if (this.selectedElement && this.mouseDown) {
       let dx = e.offsetX - this.x;
       let dy = e.offsetY - this.y;
 
@@ -47,5 +85,9 @@ export class SelectTool extends Tool {
 
     this.x = e.offsetX;
     this.y = e.offsetY;
+  }
+
+  onMouseUp(e, ctx) {
+    this.mouseDown = false;
   }
 }
